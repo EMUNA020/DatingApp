f187df2361dbe042b11ee275f87679e6c6ccfeb4
emuna <emuna020@gmail.com>
2021-11-21 11:55:09 +0200

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Logging;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
